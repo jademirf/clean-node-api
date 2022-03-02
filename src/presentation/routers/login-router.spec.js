edae4683b543cca927a25902ff5e3e301da67983
Jademir de Moura <jademir@policiacivil.pe.gov.br>
2022-03-02 15:03:04 -0300

    ensure loginRouter returns 400 if no email is provided

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,20 @@
+class LoginRouter {
+  route () {
+    return {
+      statusCode: 400
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    // sut = Single Unit Test
+    const sut = new LoginRouter()
+    const httpRequest = {
+      password: 'anything'
+    }
+    sut.route(httpRequest)
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
