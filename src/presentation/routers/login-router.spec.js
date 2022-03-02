85db19cad54aac5c81669f838e3b882db53808c4
Jademir de Moura <jademir@policiacivil.pe.gov.br>
2022-03-02 15:50:25 -0300

    create HttpResponse class

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,9 +1,25 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
-      return {
-        statusCode: 400
-      }
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.serverError()
+    }
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
+      return HttpResponse.badRequest()
+    }
+  }
+}
+
+class HttpResponse {
+  static badRequest () {
+    return {
+      statusCode: 400
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
     }
   }
 }
@@ -17,21 +33,31 @@ describe('Login Router', () => {
         password: 'anything'
       }
     }
-    sut.route(httpRequest)
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
 
   test('Should return 400 if no password is provided.', () => {
-    // sut = Single Unit Test
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
         email: 'anything@teste.com'
       }
     }
-    sut.route(httpRequest)
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest is provided.', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest has no body.', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {}
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
